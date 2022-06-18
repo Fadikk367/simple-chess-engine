8f018e5c0080d676e150c839ea8f47a60bb0baf0
Adrian Furman <fadikk367@gmail.com>
2022-06-18 23:58:46 +0200

    Add players to the game class

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,19 +1,56 @@
-import { Color } from "constants/enums";
+import { Color, GameState, PlayerType } from "constants/enums";
 import Board from "./Board";
 import GameStateManager from "./GameStateManager";
 import Move from "./Move";
 import MoveGenerator from "./MoveGenerator";
 import King from "./pieces/King";
+import Player from "./players/Player";
 
 class Game {
   board: Board;
   moveGenerator: MoveGenerator;
   gameStateManager: GameStateManager;
+  playerA: Player;
+  playerB: Player;
+  activePlayer: Player;
 
-  constructor() {
+  constructor({players}: {players: [Player, Player]}) {
     this.board = new Board();
     this.moveGenerator = new MoveGenerator(this.board);
     this.gameStateManager = new GameStateManager(this.board);
+
+    this.playerA = players[0];
+    this.playerB = players[1];
+
+    this.activePlayer = this.playerA.color === Color.White ? this.playerA : this.playerB;
+  }
+
+  get state(): GameState {
+    return this.gameStateManager.state;
+  }
+
+  play(): void {
+    do {
+      switch(this.activePlayer.type) {
+        case PlayerType.Human: {
+          console.log('Wait for input from chessboard gui...');
+          break;
+        }
+        case PlayerType.Ai: {
+          this.max();
+          break;
+        }
+      }
+      this.toggleActivePlayer();
+    } while (this.state !== GameState.Checkmate && this.state !== GameState.Stalemate);
+  }
+
+  max() {
+
+  }
+
+  min() {
+
   }
 
   minimax() {
@@ -30,6 +67,10 @@ class Game {
     const king: King = this.board.findKing(Color.White);
     const enemyMoves: Move[] = this.moveGenerator.generateForColor(Color.Black);
     console.log(this.gameStateManager.getBoardState(king, enemyMoves));
+  }
+
+  private toggleActivePlayer(): void {
+    this.activePlayer = this.activePlayer === this.playerA ? this.playerB : this.playerA;
   }
 }
 
